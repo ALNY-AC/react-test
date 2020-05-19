66374b5abea05a943fe1923eb7d5376a5e698b33
代码狮 <1173197065@qq.com>
2020-05-19 11:17:41 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.scss';
+
+class Game extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            list: ['陈芬'],
+        };
+    }
+
+
+    render() {
+        return (
+            <div className="card">
+                <div>您输入的是：{this.state.value}</div>
+                <input placeholder="请输入您要添加的员工" type="text" value={this.state.value} onChange={event => { this.setState({ value: event.target.value }) }} />
+                <button onClick={() => this.add()}>添加</button>
+                {this.state.list.map((el, i) => {
+                    return (
+                        <div className="item-card" key={i}>
+                            <div className="item">{el}</div>
+                            <div className="del" onClick={() => this.del(i)}>删除</div>
+                        </div>
+                    )
+                })}
+            </div >
+
+        );
+    }
+    del(i) {
+        this.state.list.splice(i, 1);
+        this.setState({ list: this.state.list });
+    }
+    add() {
+        if (!this.state.value) {
+            alert('姓名不能为空！');
+            return;
+        }
+        this.state.list.push(this.state.value);
+        this.setState({ list: this.state.list });
+        this.setState({ value: '' });
+    }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
